8d0aacb702c33693afa2ee95f557bf5f62dac1dd
Jorge Reyes <reyesrod.jorge@gmail.com>
2026-01-16 00:25:59 -0600

    Update Command_Center.py

--- Command_Center.py
+++ Command_Center.py
@@ -117,7 +117,7 @@ app_html = f"""
   .nav-wrap {{
     position: fixed;
     top: 42%;
-    right: 13%;
+    right: 12%;
     left: auto;
     transform: translate(-4px, 150px);
     z-index: 999999;   /* arriba del video */
